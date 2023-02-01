1cfea04e919d8cc90f05c29e771c38096423cf89
barisskann <bars_958@hotmail.com>
2023-02-01 15:45:31 +0300

    s10-g3

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { reducer } from "./slice/ListReducer";
+import logger from "redux-logger";
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, applyMiddleware(logger));
